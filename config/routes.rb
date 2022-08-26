7d5310ff89724a9e26c73b5144b253a564864c26
rafaelpando <rafaelpando9@gmail.com>
2022-08-26 11:53:53 +0100

    heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "lists#index"
+  resources :lists, except: [:index] do
+    resources :movies do
+      resources :bookmarks
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |movie|
+  movie = Movie.new(title: Faker::Movie.title, overview: Faker::Movie.quote)
+  movie.save
+end
+
+10.times do |list|
+  list = List.create(name: Faker::Book.genre)
+end
